c9c8a5326fdad6aa9645c7c0cd6730a12765092c
Hunter Gierhart <hunter@Hunters-iMac.lan>
2022-06-25 12:21:08 -0400

    Adding final challenge files

--- Static/js/app.js
+++ Static/js/app.js
@@ -24,7 +24,7 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-var filters = [];
+var filters = {};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
@@ -41,7 +41,7 @@ function updateFilters() {
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (elementValue) {
-      filters[filterId] = elementValue;
+      filters[filterID] = elementValue;
     }
 
     else {
